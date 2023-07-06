eac43c0e5dc7366c6d8ac0e07c81dc3b55da4c00
Max test <botass740@gmail.com>
2023-07-06 17:36:36 +0700

    Update Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -36,7 +36,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
-        int expected = 0;
+        int expected = 999;
         assertEquals(actual, expected);
     }
 
